88c5da2a1b290489ccd35ecd9df10f3fdbb35082
Andrew Lund <andrew.s.lund@gmail.com>
2023-03-27 17:25:44 -0700

    Implemented getElementsByClassName and most of stringifyJSON. Fixed tests for both

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,45 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// Inputs: className as a string
+// Outputs: array of elements with className as class
+// Constraints:
+// Edge Cases: look out for text in spans
+
+// Strategy: Declare an empty collection array. For each element in DOM tree, we need to check for
+// a class of className. If so, push to collection array. Check if element has children. If so,
+// recurse over all children. Return collection array. Make sure to accountn for migration of recursive
+// array elements across execution contexts.
+
+
+var getElementsByClassName = function(className, element) {
+  if (arguments.length < 2) {
+    element = document.body;
+  }
+
+  // 1. Declare empty collection array
+  var elements = [];
+
+  // base case
+  if (element.classList.contains(className)) {
+    elements.push(element);
+  }
+
+  if (element.children) {
+    var children = element.children;
+    //iterate over children
+    for (var i = 0; i < children.length; i++) {
+      // recursively call on each child
+      var childList = getElementsByClassName(className, children[i]);
+      childList.forEach(function (child) {
+        elements.push(child);
+      });
+    }
+  }
+
+
+  // n. return collection array
+  return elements;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,67 @@
 
 // but you don't so you're going to write it from scratch:
 
+// I: any object
+// O: string of input object
+// C:
+// E:
+
+// Strategy: Get type of obj argument. Follow steps for each type to convert obj to string. Use recursion
+// to address arrays and objects.
+
+// PseudoCode:
+// declare objString variable
+// Test typeof obj
+// If typeof obj === Number --> use toString()
+// If typeof obj === String --> append to objString
+// If typeof obj === boolean
+//  if true, append "true"
+//  else append "false"
+
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var objString = '';
+
+  if (typeof obj === 'number') {
+    objString += obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    objString += '"' + obj + '"';
+  }
+
+  if (typeof obj === 'boolean') {
+    obj ? objString += 'true' : objString += 'false';
+  }
+
+  if (obj === null) {
+    objString += 'null';
+  }
+
+  if (Array.isArray(obj)) {
+    objString += '[';
+    obj.forEach(function (item, index, collection) {
+      objString += stringifyJSON(item);
+      if (!(index === collection.length - 1)) {
+        objString += ',';
+      }
+    });
+    objString += ']';
+  }
+
+  if (typeof obj === 'object') {
+    objString += '{';
+    var _keys = Object.keys(obj);
+    _keys.forEach(function(key, index, collection) {
+      objString += key + ':';
+      objString += stringifyJSON(obj[key]);
+      if (!(index === collection.length - 1)) {
+        objString += ',';
+      }
+    });
+    objString += '}';
+  }
+
+  return objString;
 };
